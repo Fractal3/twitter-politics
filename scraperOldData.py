ed8f3a8058876824e11068fdd7cc0b63494ce980
Fractal3 <rannou.christophe.22@gmail.com>
2016-11-18 17:35:03 -0500

    storage of the most recetly tweet scraped

--- scraperOldData.py
+++ scraperOldData.py
@@ -19,26 +19,30 @@ twitter = Twython(app_key=auth.api_key,
 if not os.path.exists("data/"):
     os.makedirs("data/")
 now = datetime.datetime.now()
-outfn = "%i.%i.%i" % (now.month, now.day, now.year)
+
+# lis = [738177801556217855,786281218945982465] ## oldest, newest tweet
+# fields = "id,created_at,text,user,in_reply_to_screen_name,in_reply_to_status_id,in_reply_to_user_id_str,lang,favorite_count\
+# ,place,entities,contributors,truncated,id_str,in_reply_to_user_id,source,geo,in_reply_to_status_id_str\
+# ,retweeted,retweet_count,is_quote_status,favorited,coordinates".split(",")
 
 fields = "id,created_at,text,user,lang,in_reply_to_user_id,retweeted,retweet_count,is_quote_status".split(",")
-user_fields = "followers_count,friends_count,user_id,screen_name,name".split(",")
+user_fields = "followers_count,friends_count,id,screen_name,name".split(",")
 
-f = open("mostRecentId")
-startId = long(f.readline())
-mostRecentId = startId
-f.close()
-max_id = None
+# INITIALIZE OUTPUT FILE AND WRITE HEADER ROW
+# outfp.write(str.join("\t", fields) + "\n")  # .encode("utf-8"))
 
-textBuffer = []
+mostRecentId = None
+max_id = 755848806172794881
+lis = []
 
 for i in range(0, 16):  ## iterate through all tweets
+    file = codecs.open("data/tweets_{}.csv".format(username), "a", encoding="utf-8")
+    # outfn = "data/tweets_%i.%i.%i.tsv" % (now.month, now.day, now.year)
+    outfp = codecs.open("data/raw_{}.csv".format(username), "a", encoding="utf-8")
+
     ## tweet extract method with the last list item as the max_id
     user_timeline = twitter.get_user_timeline(user_id="1339835893", count=200, include_retweets=False, max_id=max_id,
-                                              since_id=startId)
-    f = codecs.open("data/tweets_{}.tsv".format(username), "a", encoding="utf-8")
-    f2 = codecs.open("data/tweets_{}_{}.tsv".format(username,outfn), "a", encoding="utf-8")
-    lis = []
+                                              since_id=mostRecentId)
     for tweet in user_timeline:
         id = tweet['id']
         lis.append(id)  ## append tweet id's
@@ -71,28 +75,30 @@ for i in range(0, 16):  ## iterate through all tweets
                 line += "\t" + str(val)
 
         line += "\n"
-        f.write(line)
-        f2.write(line)
 
-    f.close()
-    f2.close()
+        # line = ("".join(str(e) + "\t" for e in values) + "\n")
 
-    #No new tweets from the timeline, stop the process
-    if len(lis) > 0:
-        if i==0:
-            mostRecentId = lis[0]
-        max_id = min(lis) - 1
-    else:
-        break
+        # for f in fields:
+        #     if isinstance(tweet[f], unicode):
+        #         lst.append(tweet[f])
+        #     else:
+        #         lst.append(str(tweet[f]))
+        # line = ("".join(e + "\t" for e in lst) + "\n")
+        # print line
+        outfp.write(str(tweet)+"\n")  # .encode("utf-8"))
+        file.write(line)
 
-    #Less than 200 tweets, we scrapped all, we stop the process
-    if len(lis) < 200:
-        break
+    outfp.close()
+    file.close()
+    max_id = min(lis)-1
 
+    # if len(lis) > 0:
+    #     newMostRecentId = max(lis)
+    #     if newMostRecentId == mostRecentId:
+    #         break
+    #     mostRecentId = newMostRecentId
+    # else:
+    #     break
     if i < 15:
         print("INFO : Sleeping")
         time.sleep(300)  ## 5 minute rest between api calls
-
-f = open('mostRecentId', 'w')
-f.write(str(mostRecentId))
-f.close()
